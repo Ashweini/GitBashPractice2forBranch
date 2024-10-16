c6acd494ebecb36cade6920c9d5b7624723f1000
Ashweini <ashwinirk43@gmail.com>
2024-10-16 21:22:53 +0530

    pushForGitBAshPeac

---
+++ src/test/java/Samples/test1.java
@@ -0,0 +1,9 @@
+package Samples;
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		System.out.println("I am master");
+	}
+
+}
---
+++ src/test/java/Samples/test3.java
@@ -0,0 +1,10 @@
+package Samples;
+
+public class test3 {
+
+	public static void main(String[] args) {
+		System.out.println("I am Master");
+
+	}
+
+}
